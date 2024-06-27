6e832d3d6db93d47ab95c36666e8365e915c4e2b
kanvesh <kanvesh@virtusa.com>
2024-06-27 16:23:21 +0530

    one file

--- src/main/java/com/virtusa/project/PgRental/model/PropertyPhotos.java
+++ src/main/java/com/virtusa/project/PgRental/model/PropertyPhotos.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class PropertyPhotos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int photoId;
+    private long photoId;
     private String photoFileName;
 
     @ManyToOne
