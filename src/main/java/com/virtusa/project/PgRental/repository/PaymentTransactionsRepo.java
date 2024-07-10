229b9dd989180d0ff24cc6a1fe7badfc85c23b34
kanvesh <kanvesh@virtusa.com>
2024-07-10 12:38:17 +0530

    paymen id

--- src/main/java/com/virtusa/project/PgRental/repository/PaymentTransactionsRepo.java
+++ src/main/java/com/virtusa/project/PgRental/repository/PaymentTransactionsRepo.java
@@ -2,7 +2,7 @@ package com.virtusa.project.PgRental.repository;
 
 import com.virtusa.project.PgRental.model.PaymentTransactions;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface PaymentTransactionsRepo extends JpaRepository<PaymentTransactions, Long> {
+public interface PaymentTransactionsRepo extends JpaRepository<PaymentTransactions, String> {
 
 }
 
